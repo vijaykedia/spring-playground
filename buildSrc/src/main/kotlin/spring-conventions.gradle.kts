1c839f06e622462ee37bbca94a63811385188bb3
Vijay Kedia <51047478+vijaykedia@users.noreply.github.com>
2025-05-23 04:57:49 +0530

    Initial Commit to have basic skeleton

--- buildSrc/src/main/kotlin/spring-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/spring-conventions.gradle.kts
@@ -1,25 +1,43 @@
 plugins {
+    java
     alias(libs.plugins.spring.boot)
     id("com.diffplug.spotless")
+}
+
+dependencies {
+    implementation(platform(libs.spring.boot.deps))
+    implementation(libs.jetbrains.annotations)
+
+    testImplementation("org.springframework.boot:spring-boot-test-autoconfigure")
 }
 
 spotless {
     kotlinGradle {
         target("*.gradle.kts")
         ktlint()
-        ktfmt()
+        ktfmt().googleStyle()
     }
     kotlin {
         target("src/*/kotlin/**/*.kt")
         ktlint()
-        ktfmt()
+        ktfmt().googleStyle()
     }
     java {
         target("src/*/java/**/*.java")
         googleJavaFormat()
     }
+    flexmark {
+        target("**/*.md")
+    }
 }
 
-tasks.named("build") {
-    dependsOn("spotlessApply")
+tasks.withType<JavaCompile> {
+    dependsOn(tasks.named ("spotlessApply"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
 }
